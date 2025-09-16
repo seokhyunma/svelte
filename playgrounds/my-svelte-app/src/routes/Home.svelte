105d297f2c3269606dff55682052660b44eac56c
seokhyunma <seokhyunma1@gmail.com>
2025-09-16 13:07:59 +0900

    Update Home.svelte

--- playgrounds/my-svelte-app/src/routes/Home.svelte
+++ playgrounds/my-svelte-app/src/routes/Home.svelte
@@ -1,1 +1,12 @@
+<script>
+  import { currentUser } from '../stores';
+  import { $currentUser } from 'svelte/store';
+</script>
+
+<h1>홈 페이지</h1>
+{#if $currentUser}
+  <p>환영합니다, {$currentUser.username}!</p>
+{:else}
+  <p>로그인이 필요합니다.</p>
+{/if}
 
