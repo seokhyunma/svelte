fffef2a5b630fa0f477e79fb4e89fd2962f491f5
seokhyunma <seokhyunma1@gmail.com>
2025-09-16 13:08:22 +0900

    Update Signup.svelte

--- playgrounds/my-svelte-app/src/routes/Signup.svelte
+++ playgrounds/my-svelte-app/src/routes/Signup.svelte
@@ -1,1 +1,26 @@
+<script>
+  import { signup } from '../stores';
+  import { push } from 'svelte-spa-router';
+  let username = "";
+  let password = "";
+  let error = "";
+
+  function handleSignup() {
+    try {
+      signup(username, password);
+      push("/home");
+    } catch (e) {
+      error = e.message;
+    }
+  }
+</script>
+
+<h1>회원가입</h1>
+{#if error}<p style="color:red">{error}</p>{/if}
+
+<input placeholder="아이디" bind:value={username} />
+<input type="password" placeholder="비밀번호" bind:value={password} />
+
+<button on:click={handleSignup}>가입하기</button>
+<a href="#/login">로그인</a>
 
