97a4cb6d4b8886e81d43558d3fb64eb6e46956a6
seokhyunma <seokhyunma1@gmail.com>
2025-09-16 13:08:45 +0900

    Update App.svelte

--- playgrounds/my-svelte-app/src/App.svelte
+++ playgrounds/my-svelte-app/src/App.svelte
@@ -1,1 +1,19 @@
+<script>
+  import Router from 'svelte-spa-router';
+  import Login from './routes/Login.svelte';
+  import Signup from './routes/Signup.svelte';
+  import Home from './routes/Home.svelte';
+
+  const routes = {
+    "/": Login,
+    "/login": Login,
+    "/signup": Signup,
+    "/home": Home,
+  };
+</script>
+
+<Router {routes} />
+
+npm run dev
+
 
